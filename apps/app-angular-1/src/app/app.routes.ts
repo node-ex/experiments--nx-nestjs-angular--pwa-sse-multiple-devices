7ceaa4e9116fd9e160ddb1c5eeb02c3476ac0582
Pavel Balashov <pavel.balashov1@gmail.com>
2024-07-29 16:19:43 +0200

    feat: use connected device guard

--- apps/app-angular-1/src/app/app.routes.ts
+++ apps/app-angular-1/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { NotConnectedViewComponent } from './views/not-connected-view/not-connec
 import { AdminViewComponent } from './views/admin/admin-view.component';
 import { IdleViewComponent } from './views/idle-view/idle-view.component';
 import { TriggeredViewComponent } from './views/triggered/triggered-view.component';
+import { connectedDeviceGuardFn } from './modules/devices/guards/connected-device.guard';
 
 export const appRoutes: Route[] = [
   {
@@ -16,9 +17,11 @@ export const appRoutes: Route[] = [
   {
     path: 'idle',
     component: IdleViewComponent,
+    canActivate: [connectedDeviceGuardFn],
   },
   {
     path: 'triggered',
     component: TriggeredViewComponent,
+    canActivate: [connectedDeviceGuardFn],
   },
 ];
