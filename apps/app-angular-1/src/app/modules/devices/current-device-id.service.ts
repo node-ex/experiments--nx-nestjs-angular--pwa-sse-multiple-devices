ad1826195c4b4b62e1da1e60006594d282ab14a8
Pavel Balashov <pavel.balashov1@gmail.com>
2024-07-29 10:49:48 +0200

    feat: create current device id FE service

---
+++ apps/app-angular-1/src/app/modules/devices/current-device-id.service.ts
@@ -0,0 +1,21 @@
+import { Injectable, Signal, signal } from '@angular/core';
+
+const CURRENT_DEVICE_ID_KEY = 'currentDeviceId';
+
+@Injectable()
+export class CurrentDeviceIdService {
+  private currentDeviceId = signal<string | null>(null);
+
+  getCurrentDeviceId(): Signal<string | null> {
+    if (!this.currentDeviceId()) {
+      this.currentDeviceId.set(localStorage.getItem(CURRENT_DEVICE_ID_KEY));
+    }
+
+    return this.currentDeviceId;
+  }
+
+  setCurrentDeviceId(deviceId: string): void {
+    this.currentDeviceId.set(deviceId);
+    localStorage.setItem(CURRENT_DEVICE_ID_KEY, deviceId);
+  }
+}
